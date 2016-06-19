3018457721ae2584bd335db0e950b335648b3a88
Arshdeep <arshdeep.singh86@gmail.com>
2016-06-19 19:57:35 +0530

    div 2

---
+++ src/com/topcoder/Dubs.java
@@ -0,0 +1,81 @@
+package com.topcoder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Dubs {
+	boolean isDub(long L) {
+		long l = (L % 10);
+		L = L /10;
+		return l == (L %10);
+	}
+	long getDub(long L) {
+		long l = (L % 10);
+		L = L /10;
+		return (L %10) * 10 + l;
+	}
+	public long countEx(long L, long R)
+	{
+		long res = 0;
+
+		while (L <= R && L % 100 != 0) {
+			if (isDub(L))
+				++res;
+			++L;
+		}
+		if (isDub(L) && L <= R) {
+			++res;
+		}
+		if (L < R) {
+			int d[] = {11,22,33,44,55,66,77,88,99,100};
+			
+			long r = R % 100;
+			R -= r;
+			long c2 = (R - L) / 100;
+			
+			res = res + c2 * 10;
+			
+			for (int i = 0; i < 10; ++i) {
+				if (d[i] >= r) {
+					res = res + i + ((d[i] > r) ? 0 : 1);
+					break;
+				}
+			}
+		}
+
+		return res;
+	}
+	
+	//Efficient
+	long getCount(long a) {
+		long count = 0;
+		
+		for (int i = 0; i < 10; ++i) {
+			count += ( (a - 11* i + 100) / 100);
+		}
+		
+		return count - 1;
+	}
+	
+	public long count(long L, long R)
+	{
+		return getCount(R) - getCount(L - 1);
+	}
+	@Test
+	public void test() {
+		assertTrue(count(1111111, 111111111) == 11000001 );
+		assertTrue(count(50,100) == 6 );
+		assertTrue(count(99998, 99999) == 1 );
+		assertTrue(count(38545321940L, 106671028225L) == 6812570629L);
+		assertTrue(count(10, 1000000000) == 100000000);
+		assertTrue(count(49, 101) == 6 );
+		
+		assertTrue(count(10, 22) == 2 );
+		assertTrue(count(91750002841L, 91751522033L) == 151920);
+		assertTrue(count(10, 20) == 1 );
+		assertTrue(count(10, 10) == 0 );
+	}	
+	
+
+}
---
+++ src/com/topcoder/Sunnygraphs2.java
@@ -0,0 +1,70 @@
+package com.topcoder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Sunnygraphs2 {
+	boolean[] visited;
+	int[] in;
+	boolean isCycle;
+
+	long getCycle(int start, int idx, long count) {
+		if (visited[idx]) {
+			isCycle = idx == start;
+			return count;
+		}
+		visited[idx] = true;
+		return getCycle(start, in[idx], count + 1);
+	}
+
+	public long count(int[] a) {
+		this.visited = new boolean[a.length];
+		this.in = a;
+
+		this.in = a;
+
+		long res = 1;
+		long cc = 0;
+		boolean[] visit = new boolean[a.length];
+		
+		for (int i = 0; i < a.length; ++i) {
+			if (!visit[i]) {
+				long t = getCycle(i, i, 0);
+				
+				if (isCycle) {
+					++cc;
+					res *= ((1L << t) - 1);
+					for (int j = 0; j < visit.length; ++j) {
+						visit[j] |= visited[j];
+					}
+				}
+				this.visited = new boolean[a.length];
+				isCycle = false;
+			}
+		}
+		long noncycles = 0;
+		for (boolean b : visit) {
+			if (!b) {
+				++noncycles;
+			}
+		}
+		res *= (1L << noncycles );
+		if (cc == 1) {
+			++res;
+		}
+		
+		return res;
+	}
+	@Test
+	public void test() {
+		assertTrue(count(new int[]{29,34,40,17,16,12,0,40,20,35,5,13,27,7,29,13,14,39,42,9,30,38,27,40,34,33,42,20,29,42,12,29,30,21,4,5,7,25,24,17,39,32,9}) == 6184752906240L );
+		assertTrue(count(new int[]{9,2,0,43,12,14,39,25,24,3,16,17,22,0,6,21,18,29,34,35,23,43,28,28,20,11,5,12,31,24,8,13,17,10,15,9,15,26,4,13,21,27,36,39}) == 17317308137473L );
+		assertTrue(count(new int[]{1,0}) == 4 );
+		assertTrue(count(new int[]{1,0}) == 4 );
+		assertTrue(count(new int[]{1,0,0}) == 7 );
+		assertTrue(count(new int[]{2,3,0,1,0,4,5,2,3}) == 288 );
+		assertTrue(count(new int[]{2,3,0,1,0}) == 18 );
+		assertTrue(count(new int[]{2,3,0,1}) == 9 );
+	}
+}
